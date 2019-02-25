d26bb1b1250498b1c76cd03201f2808cba411f50
Pratyush Yadav <me@yadavpratyush.com>
2019-02-25 17:36:09 +0530

    Added a common header luac.h
    It will contain the global declarations and variables shared various 
    files.

---
+++ luac.h
@@ -0,0 +1,21 @@
+#ifndef __LUAC_H__
+#define __LUAC_H__
+
+#ifdef LUAC_DEBUG
+#define ASSERT(cond, fmt, args...)	do {									\
+	if (!(cond)) {															\
+		fprintf(stderr, "%s:%d -- " fmt "\n", __func__, __LINE__, ##args);	\
+		exit(1);															\
+	}																		\
+} while (0)
+#else
+#define ASSERT(cond, fmt, args...)
+#endif
+
+#ifdef LUAC_DEBUG
+#define DPRINTF(fmt, args...) printf("%s: " fmt, __func__, ##args)
+#else
+#define DPRINTF(fmt, args...)
+#endif
+
+#endif /* __LUAC_H__ */
